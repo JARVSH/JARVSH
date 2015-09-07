e5964f2ba18e401a7d173359aa12834034f487b0
ashutoshdas96 <das@ashutoshdas.com>
2015-09-08 04:02:34 +0530

    core directory created

--- parser/KEYS.py
+++
@@ -1,9 +0,0 @@
-"""Stores the keys addressing to clients(fan,switch), state(on/off) and
-   location(kitchen, bedroom)
-"""
-
-CLIENTS = ['fan', 'fans', 'light', 'tubelight', 'bulb']
-
-LOCATION = ['bedroom', 'kitchen', 'drawing room']
-
-STATE = ['on', 'off', 'of']
